98c112ddf957bf5f1787eaeac17b9aa516b61a89
Albert Schmalbach <albertschmalbach.dev@proton.me>
2026-09-25 18:06:33 +0200

    Refacture structrure of exercise 8

--- 2ºDAM/AccesoDatos/UD1/relacion1/ejercicio6/RegistroClubes.java
+++ 2ºDAM/AccesoDatos/UD1/relacion1/ejercicio6/RegistroClubes.java
@@ -1,4 +1,5 @@
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,29 +10,45 @@ import java.util.Scanner;
 
 public class RegistroClubes {
     public static void main(String[] args) {
-        Path ruta = Path.of("datos", "clubes.txt");
+        Path carpeta = Path.of("datos");
+        Path archivo = carpeta.resolve("clubes.csv");
         try {
-            if (Files.notExists(ruta)) {
-                System.out.println("Todavía no hay un archivo de clubes.");
-            } else {
-                Files.createDirectories(ruta.getParent());
-                Scanner escaner = new Scanner(System.in);
-                System.out.println("Introduce una ID");
-                String inputId = escaner.nextLine();
-                System.out.println("Introduce un nombre:");
-                String inputNombre = escaner.nextLine();
-                System.out.println("Introduce una ciudad:");
-                String inputCiudad = escaner.nextLine();
-                escaner.close();
-                try (BufferedWriter salida = Files.newBufferedWriter(
-                        ruta, StandardCharsets.UTF_8,
-                        StandardOpenOption.CREATE,
-                        StandardOpenOption.APPEND)) {
-                    salida.write(inputId + ";" + inputNombre + ";" + inputCiudad);
-                    salida.newLine();
-                } catch (Exception e) {
-                    System.err.println("Ha habido un error. Comprueba que has introcido el formato especificado: " + e.getMessage());
+            Files.createDirectories(carpeta);
+            if (Files.notExists(archivo)) {
+                Files.createFile(archivo);
+            }
+            Scanner escaner = new Scanner(System.in);
+            boolean idRepetida = true;
+            System.out.println("Introduce una ID:");
+            String inputId = escaner.nextLine();
+            while (idRepetida) {
+                try (BufferedReader entrada = Files.newBufferedReader(
+                    archivo, StandardCharsets.UTF_8)) {
+                    String linea;
+                    while ((linea = entrada.readLine()) != null) {
+                        if (!linea.equals(inputId)) {
+                            idRepetida = false;
+                        } else {
+                            idRepetida = true;
+                        }
+                    }
                 }
+                System.out.println("Introduce una ID:");
+                inputId = escaner.nextLine();
+            }
+            System.out.println("Introduce un nombre:");
+            String inputNombre = escaner.nextLine();
+            System.out.println("Introduce una ciudad:");
+            String inputCiudad = escaner.nextLine();
+            escaner.close();
+            try (BufferedWriter salida = Files.newBufferedWriter(
+                    archivo, StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND)) {
+                salida.write(inputId + ";" + inputNombre + ";" + inputCiudad);
+                salida.newLine();
+            } catch (Exception e) {
+                System.err.println("Ha habido un error. Comprueba que has introcido el formato especificado: " + e.getMessage());
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
